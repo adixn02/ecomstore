ec20154b03e01a01e9ecd61146e2490f87f57309
Chaudhary Aditya <chaudharyaditya@192.168.1.54>
2025-01-21 15:56:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to EcomStore</h1>
+      <p>site under development...</p>
+    </div>
+  );
+}
+
+export default App;
